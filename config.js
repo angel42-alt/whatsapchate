690f6e42a67aa51c1d301f48cec52383add662ff
angel42-alt <angelehmbuh@gmail.com>
2026-03-23 23:20:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     profileImage: "https://i.imgur.com/QKamZn4.jpeg",   // ganti dengan URL gambar profil
-    background: "url('https://images2.imgbox.com/78/6c/kdSrmMP4_o.jpg') center/cover",
+    background: "url('https://i.imgur.com/7PGQf5k.jpeg') center/cover",
     whatsappLink: "https://tapthelink.cfd/bTthF",
     telegramLink: "https://tapthelink.cfd/bTthF"
 };
