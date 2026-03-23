7e6541dd54297f4cf6795dde3fb98e5000a91c4f
angel42-alt <angelehmbuh@gmail.com>
2026-03-23 16:10:21 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    profileImage: "https://i.imgur.com/QKamZn4.jpeg",   // ganti dengan URL gambar profil
+    background: "url('https://images2.imgbox.com/ba/33/Typa2nfO_o.jpg') center/cover",
+    whatsappLink: "https://tapthelink.cfd/bTthF",
+    telegramLink: "https://tapthelink.cfd/bTthF"
+};
